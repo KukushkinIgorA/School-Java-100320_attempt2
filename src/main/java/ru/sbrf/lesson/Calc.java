8e04cd7c3d563d479bed349605a6ce9e8d741957
garyn <test@test.ru>
2020-03-28 12:08:56 +0300

    F1 IKUKUSHKIN Commit

--- src/main/java/ru/sbrf/lesson/Calc.java
+++ src/main/java/ru/sbrf/lesson/Calc.java
@@ -2,7 +2,18 @@ package ru.sbrf.lesson;
 
 public class Calc {
     public int add(int a, int b) {
-        int result = a + b;
-        return result;
+        return a + b;
+    }
+
+    public int substr(int a, int b) {
+        return a - b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
     }
 }
--- src/test/java/ru/sbrf/lesson/CalcTest.java
+++ src/test/java/ru/sbrf/lesson/CalcTest.java
@@ -12,4 +12,25 @@ public class CalcTest {
 
         assertEquals(5, calc.add(2, 3));
     }
+
+    @Test
+    public void substr(){
+        Calc calc = new Calc();
+
+        assertEquals(8, calc.substr(10, 2));
+    }
+
+    @Test
+    public void mult() {
+        Calc calc = new Calc();
+
+        assertEquals(56,calc.mult(7,8));
+    }
+
+    @Test
+    public void div() {
+        Calc calc = new Calc();
+
+        assertEquals(4,calc.div(12,3));
+    }
 }
